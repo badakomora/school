f21995b802a785ab5c62e4f762e1f2f8ec8060f0
badaandrew <badakomora06@gmail.com>
2024-10-12 10:11:58 +0300

    topic styles

--- src/Components/ItemList.tsx
+++ src/Components/ItemList.tsx
@@ -2,7 +2,7 @@
 import { css } from "@emotion/react";
 const styles = {
   example: css`
-    width: 50%;
+    width: 55%;
     margin: 20px;
   `,
   list: css`
@@ -10,59 +10,79 @@ const styles = {
     padding: 0;
     list-style: none;
 
-    li {
-      display: flex;
-      flex-direction: column;
-      padding: 15px;
-      box-shadow: 0px 2px 10px lavender;
-      margin-bottom: 10px;
-      background-color: #fff;
-      border-left: 4px solid #1061b7;
-      position: relative;
-      cursor: pointer;
-      border-radius: 4px;
-      overflow: hidden;
-      transition: all 0.3s ease;
+    a {
+      text-decoration: none;
+      color: #000;
+      li {
+        display: flex;
+        flex-direction: column;
+        padding: 15px;
+        box-shadow: 0px 2px 10px lavender;
+        margin-bottom: 10px;
+        background-color: #fff;
+        border-left: 4px solid #1061b7;
+        position: relative;
+        cursor: pointer;
+        border-radius: 4px;
+        overflow: hidden;
+        transition: all 0.3s ease;
 
-      &:hover {
-        color: #fff;
-        background-color: inherit;
+        &:hover {
+          color: #fff;
+          background-color: inherit;
+
+          &:after {
+            width: 100%;
+          }
+        }
+
+        .title {
+          font-weight: bold;
+          font-size: 18px;
+          position: relative;
+          z-index: 2;
+          line-height: 24px;
+        }
+
+        .subtitle {
+          font-size: 14px;
+          opacity: 0.7;
+          position: relative;
+          z-index: 2;
+        }
 
         &:after {
-          width: 100%;
+          content: "";
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 0%;
+          height: 100%;
+          transition: width 0.5s ease;
+          z-index: 1;
         }
-      }
-
-      .title {
-        font-weight: bold;
-        font-size: 18px;
-        position: relative;
-        z-index: 2;
-        line-height: 24px;
-      }
-
-      .subtitle {
-        font-size: 14px;
-        opacity: 0.7;
-        position: relative;
-        z-index: 2;
-      }
-
-      &:after {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 0%;
-        height: 100%;
-        transition: width 0.5s ease;
-        z-index: 1;
       }
     }
   `,
-  shopping: css`
+  listing: css`
     &:after {
-      background: linear-gradient(102deg, #cc00c5 0%, #1061b7 100%);
+      background: linear-gradient(50deg, #fff 0%, #1f56c6 20%);
+    }
+  `,
+  pagination: css`
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+    span {
+      background: linear-gradient(50deg, #fff 0%, #1f56c6 20%);
+      padding: 5px;
+      border-radius: 5px;
+      color: white;
+      margin: 5px;
+      a {
+        color: #fff;
+        text-decoration: none;
+      }
     }
   `,
 };
@@ -73,23 +93,55 @@ const ItemList = () => {
         <h1>Year 1 Topics</h1>
       </div>
       <ul className="list" css={styles.list}>
-        <li className="shopping" css={styles.shopping}>
-          <span className="title">A Journey through Greek Myths: Reading.</span>
-          <span className="subtitle">5 lessons</span>
-        </li>
-        <li className="shopping" css={styles.shopping}>
-          <span className="title">A Journey through Greek Myths: Reading.</span>
-          <span className="subtitle">5 lessons</span>
-        </li>
-        <li className="shopping" css={styles.shopping}>
-          <span className="title">A Journey through Greek Myths: Reading.</span>
-          <span className="subtitle">5 lessons</span>
-        </li>
-        <li className="shopping" css={styles.shopping}>
-          <span className="title">A Journey through Greek Myths: Reading.</span>
-          <span className="subtitle">5 lessons</span>
-        </li>
+        <a href=".">
+          <li className="listing" css={styles.listing}>
+            <span className="title">
+              A Journey through Greek Myths: Reading.
+            </span>
+            <span className="subtitle">5 lessons</span>
+          </li>
+        </a>
+        <a href=".">
+          <li className="listing" css={styles.listing}>
+            <span className="title">
+              A Journey through Greek Myths: Reading.
+            </span>
+            <span className="subtitle">5 lessons</span>
+          </li>
+        </a>
+        <a href=".">
+          <li className="listing" css={styles.listing}>
+            <span className="title">
+              A Journey through Greek Myths: Reading.
+            </span>
+            <span className="subtitle">5 lessons</span>
+          </li>
+        </a>
+        <a href=".">
+          <li className="listing" css={styles.listing}>
+            <span className="title">
+              A Journey through Greek Myths: Reading.
+            </span>
+            <span className="subtitle">5 lessons</span>
+          </li>
+        </a>
+        <a href=".">
+          <li className="listing" css={styles.listing}>
+            <span className="title">
+              A Journey through Greek Myths: Reading.
+            </span>
+            <span className="subtitle">5 lessons</span>
+          </li>
+        </a>
       </ul>
+      <div css={styles.pagination}>
+        <span>
+          <a href=".">Prev</a>
+        </span>
+        <span>
+          <a href=".">Next</a>
+        </span>
+      </div>
     </div>
   );
 };
--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,22 +1,36 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+import { AiOutlineUser } from "react-icons/ai";
+import { IoLogOutOutline } from "react-icons/io5";
 const styles = {
   body: css`
-    width: 30%;
+    width: 25%;
     margin: 20px;
-    margin-top: 30px;
+    margin-top: 35px;
   `,
   span: css`
-    background: linear-gradient(102deg, #cc00c5 0%, #1061b7 100%);
+    background: linear-gradient(50deg, #fff 0%, #1f56c6 20%);
     padding: 5px;
     border-radius: 4px;
     color: white;
     margin: 5px;
+    a {
+      color: #fff;
+      text-decoration: none;
+    }
   `,
-  link: css`
-    color: white;
-    text-decoration: none;
+  qlink: css`
+    display: flex;
+    span {
+      margin-top: 6px;
+      color: #1f56c6;
+    }
+    a {
+      text-decoration: none;
+      margin: 5px;
+      color: #1f56c6;
+    }
   `,
 };
 const Sidebar = () => {
@@ -26,60 +40,59 @@ const Sidebar = () => {
         <h2>Academic Years</h2>
         <div>
           <span css={styles.span}>
-            {" "}
-            <a href="." css={styles.link}>
-              Year 7
-            </a>
+            <a href=".">Year 7</a>
           </span>
           <span css={styles.span}>
-            {" "}
-            <a href="." css={styles.link}>
-              Year 8
-            </a>
+            <a href=".">Year 8</a>
           </span>
           <span css={styles.span}>
-            {" "}
-            <a href="." css={styles.link}>
-              Year 9
-            </a>
+            <a href=".">Year 9</a>
           </span>
           <span css={styles.span}>
-            {" "}
-            <a href="." css={styles.link}>
-              Year 10
-            </a>
+            <a href=".">Year 10</a>
           </span>
           <span css={styles.span}>
-            {" "}
-            <a href="." css={styles.link}>
-              Year 11
-            </a>
+            <a href=".">Year 11</a>
           </span>
         </div>
         <h2>Quick links</h2>
         <hr />
-        <p>
-          <MdOutlineKeyboardDoubleArrowRight />
-          Word of the day.
-        </p>
-        <p>
-          <MdOutlineKeyboardDoubleArrowRight />
-          Kamusi Yangu.
-        </p>
-        <p>
-          <MdOutlineKeyboardDoubleArrowRight />
-          Hadithi Hadithi?
-        </p>
-        <p>
-          <MdOutlineKeyboardDoubleArrowRight />
-          Fasihi Simulizi.
-        </p>
+        <div css={styles.qlink}>
+          <span>
+            <MdOutlineKeyboardDoubleArrowRight />
+          </span>
+          <a href="."> Word of the day.</a>
+        </div>
+        <div css={styles.qlink}>
+          <span>
+            <MdOutlineKeyboardDoubleArrowRight />
+          </span>
+          <a href=".">Kamusi Yangu.</a>
+        </div>
+        <div css={styles.qlink}>
+          <span>
+            <MdOutlineKeyboardDoubleArrowRight />
+          </span>
+          <a href=".">Hadithi Hadithi?</a>
+        </div>
+        <div css={styles.qlink}>
+          <span>
+            <MdOutlineKeyboardDoubleArrowRight />
+          </span>
+          <a href=".">Fasihi Simulizi</a>
+        </div>
         <hr />
-        <div>
-          <p>
-            <span>Andrew Bada</span>
-          </p>
-          <p>Logout</p>
+        <div css={styles.qlink}>
+          <span>
+            <AiOutlineUser />
+          </span>
+          <a href=".">Andrew Bada</a>
+        </div>
+        <div css={styles.qlink}>
+          <span>
+            <IoLogOutOutline />
+          </span>
+          <a href=".">Logout</a>
         </div>
       </div>
     </div>
